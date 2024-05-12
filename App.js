d727b3f936b24c0d4038e0adfd97eeede4fada96
M Burak Yilmazer <mbyilmazer38@gmail.com>
2024-05-13 00:17:39 +0300

    adding flatlist for scroll courseGoals

--- App.js
+++ App.js
@@ -1,14 +1,53 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  FlatList,
+  ScrollView, //* scroll yapabilmemize imkann veren component
+} from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    //* aşağıda daha güncel bir state verisi güncelleme var. setCourseGoals([...courseGoals, enteredGoalText]);
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      {text:enteredGoalText,key:Math.random().toString()},
+    ]);
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Another Text Element</Text>
-      <Text style={styles.text}>
-        Hello World!!
-      </Text>
-      <Button title="Tap Me" />
+      <View style={styles.InputContainer}>
+        <TextInput
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          placeholder="Enter Goal"
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
+      </View>
+      <View style={styles.goalsContainer}>
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData) => {
+            return (
+              <View style={styles.goalItem}>
+                <Text style={styles.goalItemText}>{itemData.item.text}</Text>
+              </View>
+            );
+          }}
+          keyExtractor={(item, index) => {return item.key}}
+        />
+      </View>
     </View>
   );
 }
@@ -16,18 +55,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    padding: 50,
+    paddingHorizontal: 16,
   },
-  text: {
-    backgroundColor: "yellow",
-    color: "blue",
-    fontSize: 25,
-    margin: 20,
-    borderWidth: 5,
+  InputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  input: {
+    borderColor: "black",
+    borderWidth: 1,
+    marginRight: 8,
     padding: 10,
-    borderColor: "red",
-    borderRadius: 10,
+    width: "70%",
+  },
+
+  goalsContainer: {
+    flex: 4,
+  },
+  goalItem: {
+    margin: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+    padding: 8,
+  },
+  goalItemText: {
+    color: "white",
   },
 });
