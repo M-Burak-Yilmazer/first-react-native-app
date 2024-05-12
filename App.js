6f477f3dfefa3b692d366f7883acf5d506406f13
M Burak Yilmazer <mbyilmazer38@gmail.com>
2024-05-12 15:27:24 +0300

    first Commit

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Another Text Element</Text>
+      <Text style={styles.text}>
+        Hello World!!
+      </Text>
+      <Button title="Tap Me" />
     </View>
   );
 }
@@ -13,8 +16,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    backgroundColor: "yellow",
+    color: "blue",
+    fontSize: 25,
+    margin: 20,
+    borderWidth: 5,
+    padding: 10,
+    borderColor: "red",
+    borderRadius: 10,
   },
 });
